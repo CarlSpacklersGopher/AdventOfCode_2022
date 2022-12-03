da85de5cec9b0636c15862c1545fe7015fa0a6fd
Garrett Laptop <garrett.d.manship@gmail.com>
2022-12-03 01:09:42 -0600

    refactored to separate out input processing.  Updated for Part 2.  Updated tests.

--- day_01/calorie_counting.py
+++ day_01/calorie_counting.py
@@ -1,24 +1,47 @@
-def count_calories(food_list: list[str]) -> int:
-    elf_calories = [0]
+ELF_DELIMITER = -1
+
+
+def get_processed_input(file_name: str) -> list[int]:
+    
+    return_list = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            if line == '\n':
+                return_list.append(ELF_DELIMITER)
+            else:
+                return_list.append(int(line.strip()))
+    
+    return_list.append(ELF_DELIMITER) # Ensure final elf gets totaled up
+    return return_list
+
+
+def get_calories_per_elf(food_list: list[int]) -> list[int]:
+    '''
+    Returns list of calories carried by each elf in descending order
+    '''
+
+    elf_calories = []
     calorie_counter = 0
 
-    food_list_copy = food_list.copy()
-    food_list_copy.append('') # Ensure last elf gets total added up
-
-    for calories in food_list_copy:
-        calories = calories.strip()
-        # Calories added up when empty line is found
-        if not calories:
+    for calories in food_list:
+        if calories == ELF_DELIMITER:
             elf_calories.append(calorie_counter)
             calorie_counter = 0
         else:
-            calorie_counter += int(calories)
-    return max(elf_calories)
+            calorie_counter += calories
+    elf_calories.sort(reverse=True) # Descending Order
+    return elf_calories
+        
+
+def get_most_calories(calories_per_elf: list[int], num_elves: int=1) -> list[int]:
+    return calories_per_elf[:num_elves]
+
 
 if __name__ == '__main__':
     
-    calorie_list=[]
-    with open("day_01/input_pt1.txt", 'r') as f:
-        calorie_list = f.readlines()
-    print(count_calories(calorie_list))
+    calorie_list = get_processed_input("day_01/input_pt1.txt")
+    calories_per_elf = get_calories_per_elf(calorie_list)
+
+    print(f'Part 1: {get_most_calories(calories_per_elf)}')
+    print(f'Part 2: {get_most_calories(calories_per_elf, num_elves=3)}')
     
--- day_01/test_calorie_counting.py
+++ day_01/test_calorie_counting.py
@@ -1,40 +1,58 @@
 import unittest
-import calorie_counting
+import calorie_counting as cc
 
 class TestCalorieCounter(unittest.TestCase):
-    def test_example(self):
+    def test_elf_separation(self):
         '''
-        Run the example provided by the prompt
+        Ensure elf sums are calculated separately
         '''
-        calorie_list = []
-        with open("day_01/testinput_1.txt", 'r') as f:
-            calorie_list = f.readlines()
+        calorie_list = cc.get_processed_input("day_01/testinput_1.txt")
         
-        self.assertEqual(calorie_counting.count_calories(calorie_list), 24000)
+        self.assertEqual(len(cc.get_calories_per_elf(calorie_list)), 5)
 
     def test_calorie_sums(self):
         '''
         Test each elf's calories are summed correctly
         '''
-        elf1 = ['1000', '2000', '3000']
-        elf2 = ['4000']
-        elf3 = ['5000', '6000']
-        elf4 = ['7000', '8000', '9000']
-        elf5 = ['10000']
+        elf1 = [1000, 2000, 3000, cc.ELF_DELIMITER]
+        elf2 = [4000, cc.ELF_DELIMITER]
+        elf3 = [5000, 6000, cc.ELF_DELIMITER]
+        elf4 = [7000, 8000, 9000, cc.ELF_DELIMITER]
+        elf5 = [10000, cc.ELF_DELIMITER]
 
-        self.assertEqual(calorie_counting.count_calories(elf1), 6000)
-        self.assertEqual(calorie_counting.count_calories(elf2), 4000)
-        self.assertEqual(calorie_counting.count_calories(elf3), 11000)
-        self.assertEqual(calorie_counting.count_calories(elf4), 24000)
-        self.assertEqual(calorie_counting.count_calories(elf5), 10000)
+        self.assertEqual(cc.get_calories_per_elf(elf1), [6000])
+        self.assertEqual(cc.get_calories_per_elf(elf2), [4000])
+        self.assertEqual(cc.get_calories_per_elf(elf3), [11000])
+        self.assertEqual(cc.get_calories_per_elf(elf4), [24000])
+        self.assertEqual(cc.get_calories_per_elf(elf5), [10000])
 
     
     def test_elf_without_food(self):
         '''
         Test that any elf without food is counted correctly
         '''
-        elf = []
-        self.assertEqual(calorie_counting.count_calories(elf), 0)
+        elf = [cc.ELF_DELIMITER]
+        self.assertEqual(cc.get_calories_per_elf(elf), [0])
+
+    def test_calperelf_is_descending(self):
+
+        test_input = [5, 8, cc.ELF_DELIMITER, 
+                      1, 3, 2, cc.ELF_DELIMITER, 
+                      cc.ELF_DELIMITER,
+                      10, 5, 3, cc.ELF_DELIMITER]
+        
+        self.assertEqual(cc.get_calories_per_elf(test_input), [18, 13, 6, 0])
+
+    def test_get_most_calories(self):
+        calorie_list = cc.get_processed_input("day_01/testinput_1.txt")
+
+        cal_per_elf = cc.get_calories_per_elf(calorie_list)
+
+        self.assertEqual(cc.get_most_calories(cal_per_elf), [24000])
+        self.assertEqual(cc.get_most_calories(cal_per_elf, num_elves=3), [24000,11000,10000])
+        self.assertEqual(cc.get_most_calories(cal_per_elf, num_elves=100), [24000,11000,10000,6000,4000])
+
+    
 
 
 if __name__ == '__main__':
