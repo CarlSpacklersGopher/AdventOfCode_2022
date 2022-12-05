927dde29c20985f433513ce9bb88e88d5f45a902
Garrett Manship <garrett.d.manship@gmail.com>
2022-12-04 23:48:20 -0600

    quick and dirty part 2 complete

--- day_03/rucksack.py
+++ day_03/rucksack.py
@@ -4,7 +4,7 @@ PRIORITY_UNICODE_OFFSET_LOWER = 96 # puts 'a' at 1
 PRIORITY_UNICODE_OFFSET_UPPER = 64 # puts 'A' at 1
 CAPS_OFFSET = 26 # Prompt states 'A' should be at 27
 
-def get_priority_sum(filepath:str):
+def part1(filepath:str) -> int:
     '''
     '''
     with open(filepath, 'r') as file:
@@ -23,22 +23,36 @@ def get_priority_sum(filepath:str):
         
         return sum(priorities)
 
+def part2(filepath:str) -> int:
+    group_contents = []
+    group_sums = 0
+    linecount = 0
+    with open(filepath, 'r') as file:
+        for line in file:
+            linecount += 1
+            items = line.strip()
+            group_contents.append(get_item_priorities(items))
 
+            if linecount == 3:
+                common_items = group_contents[0] & group_contents[1] & group_contents[2]
+                priority = np.where(common_items)
+                group_sums += priority[0][0]
+                
+                linecount = 0
+                group_contents = []
+    return group_sums
 
-def get_item_priorities(compartment:str) -> list[bool]:
-    compartment_contents = np.array([False] * 53)
-    for character in compartment:
+def get_item_priorities(items:str) -> list[bool]:
+    contents = np.array([False] * 53)
+    for character in items:
         if character.isupper():
             # Get priority
             priority = ord(character) - PRIORITY_UNICODE_OFFSET_UPPER + CAPS_OFFSET
         else: # lowercase
             priority = ord(character) - PRIORITY_UNICODE_OFFSET_LOWER
-        compartment_contents[priority] = True
-    return compartment_contents
-
-
-
-
+        contents[priority] = True
+    return contents
 
 if __name__ == '__main__':
-    print(get_priority_sum('day_03/input.txt'))
+    print('Part 1: ' + str(part1('day_03/input.txt')))
+    print('Part 2: ' + str(part2('day_03/input.txt')))
