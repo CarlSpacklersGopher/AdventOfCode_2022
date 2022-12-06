d85757d8790577a9b71edc3837b62e88be5d5730
Garrett Laptop <garrett.d.manship@gmail.com>
2022-12-05 22:41:47 -0600

    Swapped Sectors namedtuple for Search object.  Refactored cleanup function to take a criteria function

--- day_04/cleanup.py
+++ day_04/cleanup.py
@@ -1,36 +1,56 @@
-import collections
+class Search:
+    def __init__(self, start:int, end:int):
+        self.start = start
+        self.end = end
+        self.sectors = set(range(start, end + 1))
 
-Sectors = collections.namedtuple('Sectors', ['start', 'end'])
+def cleanup(filepath:str, criteria) -> int:
+    '''
+    Reads in pairs of cleanup assignments and evaluates the pairs
+    against the provided criteria function.
 
-def cleanup(filepath:str) -> int:
-    redundant_searches = 0
+    Returns: Number of search pairs that meet criteria
+    '''
+    matching_searches = 0
     with open(filepath, 'r') as f:
         for line in f:
             line = line.strip()
-            search1, search2 = line.split(',')
+            assignment1, assignment2 = line.split(',')
 
-            sectors1 = get_sectors(search1)
-            sectors2 = get_sectors(search2)
+            search1 = get_search(assignment1)
+            search2 = get_search(assignment2)
 
-            redundant_searches += is_search_redundant(sectors1, sectors2)
+            matching_searches += criteria(search1, search2)
 
-    return redundant_searches
+    return matching_searches
         
-def get_sectors(search_str) -> Sectors:
+def get_search(search_str) -> Search:
     start, end = search_str.split('-')
-    return Sectors(int(start), int(end))
+    return Search(int(start), int(end))
 
-def is_search_redundant(sectors1:Sectors, sectors2:Sectors) -> bool:
+def is_search_redundant(search1:Search, search2:Search) -> bool:
     '''
     Returns if one sector is completely contained within the other sector
     '''
-    lowest_start = min(sectors1.start, sectors2.start)
-    highest_end = max(sectors1.end, sectors2.end)
+    sectors1 = search1.sectors
+    sectors2 = search2.sectors
+    
+    return sectors1.issubset(sectors2) or sectors2.issubset(sectors1)
 
-    test_sector = Sectors(lowest_start, highest_end)
+def is_search_overlapping(search1:Search, search2:Search) -> bool:
+    '''
+    Returns if there is any overlap between the two searches
+    '''
+    sectors1 = search1.sectors
+    sectors2 = search2.sectors
 
-    return (test_sector == sectors1) or (test_sector == sectors2)
+    # Returns true if any items in one set are in the other
+    return not sectors1.isdisjoint(sectors2)
+
 
 if __name__ == '__main__':
-    redundancies = cleanup('day_04/input.txt')
+    redundancies = cleanup('day_04/input.txt', is_search_redundant)
     print(f'Part 1: {redundancies}')
+
+    overlaps = cleanup('day_04/input.txt', is_search_overlapping)
+    print(f'Part 2: {overlaps}')
