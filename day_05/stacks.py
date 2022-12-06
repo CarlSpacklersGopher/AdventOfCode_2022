5e8ed3a8c1a1b55a280d7da8f06ef48bdf573f10
Garrett Laptop <garrett.d.manship@gmail.com>
2022-12-05 23:25:52 -0600

    skeleton of functions created

--- day_05/stacks.py
+++ day_05/stacks.py
@@ -1,4 +1,43 @@
+from dataclasses import dataclass
+
+@dataclass
+class Move:
+    self.source: int # stack number
+    self.destination: int # stack number
+    self.boxes: int # num boxes to move from source to destination
+
+def get_starting_stacks(filepath:str) -> list[list]:
+    '''
+    Reads the provided input file - returns list of package stacks
+    '''
+    pass
+
+def get_moves(filepath:str) -> list[Move]:
+    '''
+    Reads the provided input file - returns list of package moves
+    '''
+    pass
+
+def move_package(stacks:list, instruction:Move):
+    '''
+    Moves package from one stack to another based on instruction
+    '''
+    pass
+
+def get_top_boxes(stacks:list) -> str:
+    '''
+    Returns the box on top of each stack, in order
+    '''
+    pass
+
+def rearrange_crates(filepath:str) -> str:
+    '''
+    Rearranges the stacks of crates based on the list of instructions
+    Returns string of the top crate from each stack, in order
+    '''
+
 if __name__ == '__main__':
+    filepath = 'day_05/testinput.txt'
     pt1 = None
     print(f'Part 1: {pt1}')
 
--- day_05/test_stacks.py
+++ day_05/test_stacks.py
@@ -3,7 +3,22 @@ import stacks as s
 
 class TestStacks(unittest.TestCase):
 
-    def test_(self):
+    def test_stack_read(self):
+        pass
+
+    def test_instruction_read(self):
+        pass
+
+    def test_move1package(self):
+        pass
+    
+    def test_move_multiple_packages(self):
+        pass
+
+    def test_top_boxes(self):
+        pass
+
+    def test_rearrange(self):
         pass
 
 
