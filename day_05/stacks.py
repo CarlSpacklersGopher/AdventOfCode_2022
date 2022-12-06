3f5048ef42ffaeb2ef9c354d817cab1190556d1d
Garrett Laptop <garrett.d.manship@gmail.com>
2022-12-05 22:50:27 -0600

    adding day 5 skeleton

---
+++ day_05/stacks.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    pt1 = None
+    print(f'Part 1: {pt1}')
+
+    pt2 = None
+    print(f'Part 2: {pt2}')
---
+++ day_05/test_stacks.py
@@ -0,0 +1,11 @@
+import unittest
+import stacks as s
+
+class TestStacks(unittest.TestCase):
+
+    def test_(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
