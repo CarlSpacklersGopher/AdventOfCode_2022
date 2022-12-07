a398c8c06ad29885be5d6f2c580c6bf799a1b477
Garrett Manship <garrett.d.manship@gmail.com>
2022-12-07 12:56:00 -0600

    wrote test cases

--- day_05/test_stacks.py
+++ day_05/test_stacks.py
@@ -4,22 +4,85 @@ import stacks as s
 class TestStacks(unittest.TestCase):
 
     def test_stack_read(self):
-        pass
+        actual_stacks = s.get_starting_stacks('day_05/testinput.txt')
+        anticipated_stacks = [
+            [],
+            ['Z', 'N'],
+            ['M', 'C', 'D'],
+            ['P']
+        ]
+        self.assertEqual(actual_stacks[0], anticipated_stacks[0])
+        self.assertEqual(actual_stacks[1], anticipated_stacks[1])
+        self.assertEqual(actual_stacks[2], anticipated_stacks[2])
+        self.assertEqual(actual_stacks[3], anticipated_stacks[3])
 
     def test_instruction_read(self):
-        pass
+        move1 = s.Move(source=2, destination=1, boxes=1)
+        move2 = s.Move(source=1, destination=3, boxes=3)
+        move3 = s.Move(source=2, destination=1, boxes=2)
+        move4 = s.Move(source=1, destination=2, boxes=1)
+        
+        actual_moves = s.get_moves('day_05/testinput.txt')
+
+        self.assertEqual(actual_moves[0], move1)
+        self.assertEqual(actual_moves[1], move2)
+        self.assertEqual(actual_moves[2], move3)
+        self.assertEqual(actual_moves[3], move4)
 
     def test_move1package(self):
-        pass
+        stacks = [
+            [],
+            ['Z', 'N'],
+            ['M', 'C', 'D'],
+        ]
+
+        anticipated_stacks = [
+            [],
+            ['Z'],
+            ['M', 'C', 'D', 'N'],
+        ]
+
+        move = s.Move(source=1, destination=2, boxes=1)
+        s.move_package = (stacks, move)
+
+        self.assertEqual(stacks[1], anticipated_stacks[1])
+        self.assertEqual(stacks[2], anticipated_stacks[2])
     
     def test_move_multiple_packages(self):
-        pass
+        stacks = [
+            [],
+            ['Z', 'N'],
+            ['M', 'C', 'D'],
+        ]
+
+        anticipated_stacks = [
+            [],
+            ['Z', 'N', 'D', 'C'],
+            ['M'],
+        ]
+
+        move = s.Move(source=2, destination=1, boxes=2)
+        s.move_package = (stacks, move)
+
+        self.assertEqual(stacks[1], anticipated_stacks[1])
+        self.assertEqual(stacks[2], anticipated_stacks[2])
 
     def test_top_boxes(self):
-        pass
+        stacks = [
+            [],
+            ['Z', 'N'],
+            ['M', 'C', 'D'],
+            ['P']
+        ]
+
+        actual_top_boxes = s.get_top_boxes(stacks)
+
+        self.assertEqual(actual_top_boxes, 'NDP')
 
     def test_rearrange(self):
-        pass
+        actual_top_boxes = s.rearrange_crates('day_05/testinput.txt')
+
+        self.assertEqual(actual_top_boxes, 'CMZ')
 
 
 if __name__ == '__main__':
