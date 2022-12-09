0bdeb0533c0c75e1b97f28d762c9ce1bd456c70d
Garrett Manship <garrett.d.manship@gmail.com>
2022-12-08 22:57:49 -0600

    day 6 part 2 passed

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -1,17 +1,14 @@
 import re
 
-def find_message_start(datastream:str) -> int:
+def find_message_start(datastream:str, marker_length:int) -> int:
     '''
     Given datastream, returns the index that the message starts (not including the marker)
     '''
-    marker_length = 4
     message_start = -1
     for i in range(len(datastream)):
         potential_marker = datastream[i: i + marker_length]
         if len(set(potential_marker)) == marker_length: # unique
-            message_start = i + marker_length
-            break
-    return message_start
+            return i + marker_length
 
 
 if __name__ == '__main__':
@@ -19,9 +16,9 @@ if __name__ == '__main__':
     with open('day_06/input.txt', 'r') as f:
         datastream = f.readline().strip() # only 1 line
     
-    pt1 = find_message_start(datastream)
+    pt1 = find_message_start(datastream, 4)
     print('Part 1: ' + str(pt1))
 
-    pt2 = None
+    pt1 = find_message_start(datastream, 14)
     print('Part 1: ' + str(pt1))
     
--- day_06/test_day_06.py
+++ day_06/test_day_06.py
@@ -5,15 +5,19 @@ import day_06
 class TestDay06(unittest.TestCase):
 
     def test_day_06_pt1(self):
-        self.assertEqual(day_06.find_message_start('mjqjpqmgbljsphdztnvjfqwrcgsmlb'), 7)
-        self.assertEqual(day_06.find_message_start('bvwbjplbgvbhsrlpgdmjqwftvncz'), 5)
-        self.assertEqual(day_06.find_message_start('nppdvjthqldpwncqszvftbrmjlhg'), 6)
-        self.assertEqual(day_06.find_message_start('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 10)
-        self.assertEqual(day_06.find_message_start('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 11)
+        self.assertEqual(day_06.find_message_start('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4), 7)
+        self.assertEqual(day_06.find_message_start('bvwbjplbgvbhsrlpgdmjqwftvncz', 4), 5)
+        self.assertEqual(day_06.find_message_start('nppdvjthqldpwncqszvftbrmjlhg', 4), 6)
+        self.assertEqual(day_06.find_message_start('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4), 10)
+        self.assertEqual(day_06.find_message_start('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4), 11)
         
 
     def test_day_06_pt2(self):
-        pass
+        self.assertEqual(day_06.find_message_start('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14), 19)
+        self.assertEqual(day_06.find_message_start('bvwbjplbgvbhsrlpgdmjqwftvncz', 14), 23)
+        self.assertEqual(day_06.find_message_start('nppdvjthqldpwncqszvftbrmjlhg', 14), 23)
+        self.assertEqual(day_06.find_message_start('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14), 29)
+        self.assertEqual(day_06.find_message_start('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14), 26)
 
 if __name__ == '__main__':
     unittest.main()
