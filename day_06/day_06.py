c38c0a80a3546c1d1e115c28621d8b31e7164253
Garrett Manship <garrett.d.manship@gmail.com>
2022-12-08 20:49:33 -0600

    created day 6 skeleton

---
+++ day_06/day_06.py
@@ -0,0 +1,11 @@
+
+
+
+
+if __name__ == '__main__':
+    pt1 = None
+    print('Part 1: ' + str(pt1))
+
+    pt2 = None
+    print('Part 1: ' + str(pt1))
+    
---
+++ day_06/test_day_06.py
@@ -0,0 +1,14 @@
+
+import unittest
+import day_06
+
+class TestDay06(unittest.TestCase):
+
+    def test_day_06_pt1(self):
+        pass
+
+    def test_day_06_pt2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
