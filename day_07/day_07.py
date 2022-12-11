4d46aa6f89f2c69a01f37e965785cb93f692d2b7
Garrett Laptop <garrett.d.manship@gmail.com>
2022-12-11 11:19:30 -0600

    node class and read from file to build tree implemented

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -1,86 +1,134 @@
 
 class Node:
-    name = ''
-    size = 0
-    children = []
-    children_edited = False
-    
-    def __init__(self, name:str):
-        self.name = name
 
-    def add_child(self, child:Node):
+    _indents = 0
+    
+    def __init__(self, name:str, size:int=0):
+        self.name = name
+        self._size = size
+        self.children = []
+        self.children_edited = False
+
+    def add_child(self, child):
         self.children.append(child)
         self.children_edited = True
 
-    def remove_child(self, child:Node):
+    def remove_child(self, child):
         self.children.remove(child)
         self.children_edited = True
 
-    def get_child(self, child_name:str) -> Node:
+    def get_child(self, path:str):
+        path_parts = path.split('/')
+        child_name = path_parts[1]
+        desired_node_name = path_parts[-1]
         for child in self.children:
-            if child.name == child_name:
+            if desired_node_name == child.name:
                 return child
-        return None
+            elif child.name == child_name:
+                child_rel_path = '/'.join(path_parts[1:])
+                return child.get_child(child_rel_path)
 
-    def get_size(self, min_file_size:int = 0) -> int:
+    def get_size(self, max_file_size:int = -1) -> int:
         '''
-        Get size of Node and all children greater than min_file_size
+        Get size of Node and all children <= max_file_size
         '''
+
         if self.children_edited:
-            self._recalculate_size(self)
-        if self.size >= min_file_size:
-            return self.size
-        return 0
+            self._recalculate_size(max_file_size)
+            return self._size
 
-    def _recalculate_size(self):
+        if (self._size <= max_file_size) or (max_file_size == -1):
+            return self._size
+        else:
+            return 0
+
+    def _recalculate_size(self, max_file_size:int):
         self.children_edited = False
-        self.size = 0
-        for child in self.children():
-            self.size += child.get_size(min_file_size)
+        self._size = 0
+        for child in self.children:
+            self._size += child.get_size(max_file_size)
+
+    def __eq__(self, other):
+        return self.name == other.name and \
+               self.get_size() == other.get_size() and \
+               self.children == other.children
+
+    def __str__(self):
+        lines = []
+        is_dir = bool(self.children)
+        indent = '  ' * self._indents
+        if is_dir:
+            node_description = '(dir)'
+        else:
+            node_description = f'(file, size={self._size})'
+        lines.append(indent + f'- {self.name} {node_description}')
+        for child in self.children:
+            child._indents = self._indents + 1
+            lines.append(str(child))
+
+        self._indents = 0
+        return '\n'.join(lines)
+
+    def _get_child_str(self, indents):
+        for child in self.children:
+            pass
+            
 
 
-    def set_size(self, file_size:int) -> bool:
-        '''
-        sets the size attribute of node.  Only valid on Nodes without children.
-        '''
-        if self.children:
-            return False
-        self.size = file_size
-        return True
         
-
+ROOT = None
 
 def build_file_tree(filepath:str) -> Node:
     '''
     builds file tree object given a file of terminal output.  Returns root node of tree.
     '''
+    global ROOT
     terminal_output = []
     with open(filepath, 'r') as f:
         terminal_output = f.readlines()
-    
+
     current_dir = ''
-    parent = None
-    #TODO: Need to track parent node.
-    # Alternatively, use Node.get_child to find child matching current_dir
+    current_node = None
     for line in terminal_output:
         pieces = line.split()
         if pieces[0] == '$':
             command = pieces[1]
             if command == 'cd':
                 goto_dir = pieces[2]
-                if goto_dir == '..':
-                    # remove last directory
-                    current_dir = current_dir[:current_dir.rfind('/')]
+                if goto_dir == '/':
+                    if not ROOT: 
+                        ROOT = Node('/')
+                    current_dir = '/'
+                    current_node = ROOT
+                elif goto_dir == '..':
+                    num_slashes = current_dir.count('/')
+                    if num_slashes == 1:
+                        # currently in a child of root, but no further
+                        current_node = ROOT
+                        current_dir = '/'
+                    else:
+                        # remove last directory
+                        new_dir = current_dir[:current_dir.rfind('/')]
+                        current_dir = new_dir
+                        current_node = ROOT.get_child(new_dir)
                 else:
-                    current_dir = f'{current_dir}/{goto_dir}'
+                    if current_dir == ROOT.name:
+                        dir_pieces = ['']
+                    else:
+                        dir_pieces = current_dir.split('/') 
+                    dir_pieces.append(goto_dir)
+                    current_dir = '/'.join(dir_pieces)
+                    current_node = ROOT.get_child(current_dir)
 
         elif pieces[0] == 'dir':
             dir_node = Node(pieces[1])
-            parent.add_child(dir_node)
-        elif int(pieces[0]):
-            file_node = Node(pieces[1])
-            file_node.set_size(int(pieces[0]))
-            parent.add_child(file_node)
+            current_node.add_child(dir_node)
+        else: # file object
+            file_size = int(pieces[0])
+            file_name = pieces[1]
+
+            file_node = Node(file_name, file_size)
+            current_node.add_child(file_node)
 
 
 
--- day_07/test_day_07.py
+++ day_07/test_day_07.py
@@ -4,8 +4,82 @@ import day_07
 
 class TestDay07(unittest.TestCase):
 
+    def setUp(self):
+        # Build Tree
+        self.ROOT = day_07.Node('/')
+        self.a = day_07.Node('a')
+        self.e = day_07.Node('e')
+        self.i = day_07.Node('i', 584)
+        self.f = day_07.Node('f', 29116)
+        self.g = day_07.Node('g', 2557)
+        self.h_lst = day_07.Node('h.lst', 62596)
+        self.b_txt = day_07.Node('b.txt', 14848514)
+        self.c_dat = day_07.Node('c.dat', 8504156)
+        self.d = day_07.Node('d')
+        self.j = day_07.Node('j', 4060174)
+        self.d_log = day_07.Node('d.log', 8033020)
+        self.d_ext = day_07.Node('d.ext', 5626152)
+        self.k = day_07.Node('k', 7214296)
+
+        self.ROOT.add_child(self.a)
+        self.ROOT.add_child(self.b_txt)
+        self.ROOT.add_child(self.c_dat)
+        self.ROOT.add_child(self.d)
+
+        self.a.add_child(self.e)
+        self.a.add_child(self.f)
+        self.a.add_child(self.g)
+        self.a.add_child(self.h_lst)
+        
+        self.e.add_child(self.i)
+
+        self.d.add_child(self.j)
+        self.d.add_child(self.d_log)
+        self.d.add_child(self.d_ext)
+        self.d.add_child(self.k)
+    
+    def test_tree_build(self):
+        day_07.build_file_tree('day_07/testinput.txt')
+        self.assertEqual(day_07.ROOT, self.ROOT)
+
+    def test_get_child(self):
+        # Get direct child
+        self.assertEqual(self.e.get_child('e/i'), self.i) # only child
+        self.assertEqual(self.ROOT.get_child('/d'), self.d) # child dir
+        self.assertEqual(self.a.get_child('a/g'), self.g) # child file
+
+        # Get distant child
+        self.assertEqual(self.ROOT.get_child('/a/e/i'), self.i) # only child
+        self.assertEqual(self.ROOT.get_child('/a/e'), self.e) # child dir
+        self.assertEqual(self.ROOT.get_child('/d/d.log'), self.d_log) # child file
+    
+    def test_size(self):
+        # test single file
+        self.assertEqual(self.i.get_size(), 584) # return size no matter what
+        self.assertEqual(self.i.get_size(583), 0) # limit < size
+        self.assertEqual(self.i.get_size(585), 584) # limit > size
+
+        # test one layer directory
+        dirsize = 4_060_174 + 8_033_020 + 5_626_152 + 7_214_296
+        self.assertEqual(self.d.get_size(), dirsize)
+
+        self.d.children_edited = True # force trigger of recalculation
+        self.assertEqual(self.d.get_size(8_000_000), dirsize - 8_033_020)
+
+        # test multi-layer directory
+        dirsize = 584 + 29_116 + 2557 + 62_596
+        self.assertEqual(self.a.get_size(), dirsize)
+    
+
     def test_day_07_pt1(self):
-        pass
+        print('e:\n')
+        print(self.e)
+
+        print('a:\n')
+        print(self.a)
+
+        print('ROOT:\n')
+        print(self.ROOT)
 
     def test_day_07_pt2(self):
         pass
