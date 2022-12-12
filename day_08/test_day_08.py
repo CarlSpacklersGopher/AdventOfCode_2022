e6a16168bb7164b345bc2978f2350d2cc709a817
Garrett Manship <garrett.d.manship@gmail.com>
2022-12-12 17:46:13 -0600

    day 8 file creation

---
+++ day_08/test_day_08.py
@@ -0,0 +1,14 @@
+
+import unittest
+import day_08
+
+class TestDay08(unittest.TestCase):
+
+    def test_day_08_pt1(self):
+        pass
+
+    def test_day_08_pt2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
