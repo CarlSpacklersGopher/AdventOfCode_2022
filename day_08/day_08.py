cd5362d8f2d30866b66a6d84fb39e96fbfc0451f
Garrett Manship <garrett.d.manship@gmail.com>
2022-12-13 07:26:56 -0600

    initial skeleton of functions and tests

--- day_08/day_08.py
+++ day_08/day_08.py
@@ -1,4 +1,46 @@
+import numpy as np
 
+def get_tree_heights_from_file(file:str) -> list[list[int]]:
+    '''
+    Reads the provided file of tree heights into a 2D list of tree heights
+    '''
+    pass
+
+def get_tree_visibilities(heights:list[int]) -> list[bool]:
+    '''
+    Determines if each listed tree is visible
+    '''
+    pass
+
+def get_visibility_from_north(tree_heights:list[list[int]]) -> list[list[bool]]:
+    '''
+    Generates matrix of booleans that indicates if each tree is visible from north, looking south.
+    '''
+    pass
+
+def get_visibility_from_south(tree_heights:list[list[int]]) -> list[list[bool]]:
+    '''
+    Generates matrix of booleans that indicates if each tree is visible from south, looking north.
+    '''
+    pass
+
+def get_visibility_from_east(tree_heights:list[list[int]]) -> list[list[bool]]:
+    '''
+    Generates matrix of booleans that indicates if each tree is visible from east, looking west.
+    '''
+    pass
+
+def get_visibility_from_west(tree_heights:list[list[int]]) -> list[list[bool]]:
+    '''
+    Generates matrix of booleans that indicates if each tree is visible from west, looking east.
+    '''
+    pass
+
+def count_visible_trees(tree_heights:list[list[int]]) -> int:
+    '''
+    Returns a count of the trees visible from any direction
+    '''
+    pass
 
 
 
--- day_08/test_day_08.py
+++ day_08/test_day_08.py
@@ -4,6 +4,24 @@ import day_08
 
 class TestDay08(unittest.TestCase):
 
+    def test_file_read(self):
+        pass
+
+    def test_tree_visibility(self):
+        pass
+
+    def test_visibility_from_north(self):
+        pass
+    
+    def test_visibility_from_south(self):
+        pass
+    
+    def test_visibility_from_east(self):
+        pass
+    
+    def test_visibility_from_west(self):
+        pass
+
     def test_day_08_pt1(self):
         pass
 
