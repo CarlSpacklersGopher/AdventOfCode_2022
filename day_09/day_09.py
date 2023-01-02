82373a0d1bd40ce1f50ec687ad7e5a827d4c0275
Garrett Laptop <garrett.d.manship@gmail.com>
2023-01-02 13:48:21 -0600

    day 9 skeleton

---
+++ day_09/day_09.py
@@ -0,0 +1,12 @@
+
+def main():
+    pt1 = None
+    print('Part 1: ' + str(pt1))
+
+    pt2 = None
+    print('Part 2: ' + str(pt2))
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ day_09/test_day_09.py
@@ -0,0 +1,14 @@
+
+import unittest
+import day_09
+
+class TestDay09(unittest.TestCase):
+
+    def test_day_09_pt1(self):
+        pass
+
+    def test_day_09_pt2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
