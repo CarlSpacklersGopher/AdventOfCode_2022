1ebc7bb48ef9deaffa13a7061cb8cacfe0b4f74c
Garrett Laptop <garrett.d.manship@gmail.com>
2023-01-02 13:47:47 -0600

    updated template to define main() as a separate function

--- create_day_utility.py
+++ create_day_utility.py
@@ -21,14 +21,16 @@ if __name__ == '__main__':
 
     contents_dayx = '''
 
-
-
-if __name__ == '__main__':
+def main():
     pt1 = None
     print('Part 1: ' + str(pt1))
 
     pt2 = None
     print('Part 2: ' + str(pt2))
+
+
+if __name__ == '__main__':
+    main()
     '''
 
 
