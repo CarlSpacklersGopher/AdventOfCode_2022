8dda6e92fd8626be56939ca91cb80b5f48b0255d
Garrett Manship <garrett.d.manship@gmail.com>
2023-01-06 20:13:26 -0600

    initial day 10 skeleton commit

---
+++ day_10/test_day_10.py
@@ -0,0 +1,14 @@
+
+import unittest
+import day_10
+
+class TestDay10(unittest.TestCase):
+
+    def test_day_10_pt1(self):
+        pass
+
+    def test_day_10_pt2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
