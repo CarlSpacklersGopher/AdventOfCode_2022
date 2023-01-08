fd3f161da0cf7d1090acdde7e79e61a70d7df954
Garrett Laptop <garrett.d.manship@gmail.com>
2023-01-07 21:19:07 -0600

    initial skeleton of day 11

---
+++ day_11/day_11.py
@@ -0,0 +1,21 @@
+
+
+def process_input(filepath: str) -> list:
+    input = []
+    with open(filepath, encoding='utf-8', mode='r') as f:
+        for line in f:
+            pass
+
+    return input
+
+def main():
+    pt1 = None
+    print('Part 1: ' + str(pt1))
+
+    pt2 = None
+    print('Part 2: ' + str(pt2))
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ day_11/test_day_11.py
@@ -0,0 +1,14 @@
+
+import unittest
+import day_11
+
+class TestDay11(unittest.TestCase):
+
+    def test_day_11_pt1(self):
+        pass
+
+    def test_day_11_pt2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
