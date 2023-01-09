5e6369d5b2b34583f52b662b25eb6520efc3e4a5
Garrett Manship <garrett.d.manship@gmail.com>
2023-01-08 21:59:30 -0600

    initial skeleton of day 12

---
+++ day_12/test_day_12.py
@@ -0,0 +1,14 @@
+
+import unittest
+import day_12
+
+class TestDay12(unittest.TestCase):
+
+    def test_day_12_pt1(self):
+        pass
+
+    def test_day_12_pt2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
